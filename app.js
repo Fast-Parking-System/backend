b4c4facac9f1ff9038decc5ec1a55c31ecb711b2
Naura <naurazp26@gmail.com>
2023-11-30 18:07:29 +0700

    tambah scrip deploy

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const router = require('./routes/index.js');
 
+app.use(cors());
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use('/api', router);
