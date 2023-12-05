3abd1f58875b51063eb13a406dc52b7668c08e1c
Naura <naurazp26@gmail.com>
2023-12-05 19:08:19 +0700

    dokumentasi

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ const swaggerDocument = YAML.parse(file);
 const swaggerUi = require('swagger-ui-express');
 const app = express();
 
-const router = require('./routes/index.js');
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
 
+const router = require('./routes/index.js');
 app.use('/api', router);
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', function (req, res) {
@@ -26,18 +24,6 @@ app.get('/', function (req, res) {
         error: null,
         data: null
     });
-});
-
-app.get('/docs', async function (req, res, next) {
-    try {
-        const filePath = './request.http';
-        const fileContentBuffer = await fs.readFileSync(filePath, { encoding: 'utf-8' });
-        const formattedContent = `<pre>${fileContentBuffer}</pre>`;
-        res.send(formattedContent);
-        // res.send(fileContentBuffer);
-    } catch (err) {
-        next(err);
-    }
 });
 
 const PORT = process.env.PORT;
