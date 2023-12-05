4cb8542a2707f55068bc696585756ba2172905bc
Naura <naurazp26@gmail.com>
2023-12-05 19:06:57 +0700

    dokumentasi

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const YAML = require('yaml');
+const swaggerDocument = YAML.parse(file);
+const swaggerUi = require('swagger-ui-express');
 const app = express();
 
 const router = require('./routes/index.js');
@@ -10,7 +14,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
+
 app.use('/api', router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', function (req, res) {
     res.json({
