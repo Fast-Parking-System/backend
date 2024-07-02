182d3a2ae06f39fdfbc05c6496ee661a3898a6aa
Magdalena <rezapardosi27@gmail.com>
2024-07-02 10:43:31 +0700

    add console error

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+    console.log(err);
     return res.status(500).json({
         status: false,
         message: 'somethink broke!',
